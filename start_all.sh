0dcd034a127ffe2b1a45ad78add70bcf2c215137
tauwoo_server <tauwoo@seuxw.cc>
2018-08-07 16:31:10 +0800

    Server Bugfix

--- start_all.sh
+++ start_all.sh
@@ -2,4 +2,4 @@
 
 nohup python3 crawler_main.py lianjia > log/lianjia.log &
 nohup python3 crawler_main.py ziroom > log/ziroom.log &
-nohup python3 crawler_main.py qk365api > log/qk.log &
+nohup python3 crawler_main.py qk > log/qk.log &
